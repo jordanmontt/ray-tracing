7c4270a866a1aea5691dd33da578b4b7418ce067
jmsebastian <sebastianjmt@hotmail.com>
2019-03-28 13:45:05 -0400

    updated plane

--- Plane.cpp
+++ Plane.cpp
@@ -28,7 +28,7 @@ bool Plane::isImpact(const Ray &ray, double &t, Vector3D &n, Point3D &q) const
 {
     double numerator = (this->p - ray.origin) * this->normal;
     double denominator = ray.direction * this->normal;
-    if (numerator == 0 && denominator == 0)
+    if (denominator == 0)
         return false;
     t = numerator / denominator;
     q = ray.origin + t * ray.direction;
